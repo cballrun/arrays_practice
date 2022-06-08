fb6713dbffcee7ed3a57e5de446c66f3fab1d1a5
Carter Ball <carter.k.ball@gmail.com>
2022-06-08 12:45:43 -0400

    Add variables of different data types

---
+++ arrays.rb
@@ -0,0 +1,17 @@
+# Arrays Practice
+
+## String Variables
+
+friends = ["Paul", "Chuck", "Jack", "J", "Chase"]
+
+## Integer Variables
+
+steak_weight = [4, 8, 12, 20]
+
+## Float Variables
+
+steak_weight_pounds = [0.25, 0.5, 0.75, 1.25]
+
+## Boolean Variables
+
+large_steak = [if >.5 = true, if <.5 = false, if =.5 =false, if 1.25 =  true]
