3b12ad31e06b5093285fd0ef49947d24405779de
Carter Ball <carter.k.ball@gmail.com>
2022-06-08 14:10:34 -0400

    Add 4 methods acting on the variables created earlier and explain what they are doing

--- arrays.rb
+++ arrays.rb
@@ -15,3 +15,30 @@ steak_weight_pounds = [0.25, 0.5, 0.75, 1.25]
 ## Boolean Variables
 
 large_weight = [5 = true, 20 = true, 1 = false, 2 = false]
+
+
+## Pop Method
+
+#The below method and command will return the value "Chase" and then remove it from the array. The command after that will show the array with "Chase" removed
+
+ friends.pop
+ puts friends
+
+ ## Push Method
+
+#The below method will return the steak_weight array and add the integer 24 to it
+
+steak_weight.push 24
+
+## Shift method
+
+#The below method will remove the boolean 5 = true from the array large_weight and return the value 5 = true
+
+large_weight.shift
+
+
+## Unshift method
+
+The below method will add 2 to the beginning of the steak_weight_pounds Arrays
+
+steak_weight_pounds.unshift 24
